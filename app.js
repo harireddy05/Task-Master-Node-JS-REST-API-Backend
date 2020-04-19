399705576d203ee4268d91b6b521f58dadae0fde
Hari <harireddy05@gmail.com>
2020-04-19 03:49:25 -0500

    UI Base Functionality Tested

--- app.js
+++ app.js
@@ -23,29 +23,42 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     let Task = mongoose.model('Task', { id: Object, title: String, isComplete: Boolean });
-    // const newtask = new Task({ id: uuid.v4(), title: 'Rise and shine', isComplete: true });
-    // newtask.save().then(() => console.log("Initial Task Added."))
+    const newtask = new Task({ id: uuid.v4(), title: 'Rise and shine', isComplete: true });
+    newtask.save().then(() => console.log("Initial Task Added."))
 });
 
 app.get('/api/tasks', (req, res) => {
-    let cursor = db.collection('tasks').find({});
-    cursor.toArray((err, result) => {
-        if (err) throw err;
-        res.json(result)
-    })
+    try {
+        // console.log(req)
+        let cursor = db.collection('tasks').find({});
+        cursor.toArray((err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 app.post('/addtask', jsonParser, (req, res) => {
-    let Task = mongoose.model('Task');
-    let addtask = new Task({ id: uuid.v4(), title: req.body.title, isComplete: req.body.isComplete });
-    addtask.save().then(() => console.log("Task added."))
-    res.send("Task added.")
+    try {
+        let Task = mongoose.model('Task');
+        let addtask = new Task({ id: uuid.v4(), title: req.body.title, isComplete: req.body.isComplete });
+        addtask.save().then(() => console.log("Task added."))
+        res.send("Task added.")
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 app.post('/deletetask', jsonParser, (req, res) => {
-    const Task = mongoose.model('Task');
-    Task.find({"id": req.body.id }).deleteOne().exec();
-    res.send("Task deleted.")
+    try {
+        const Task = mongoose.model('Task');
+        Task.find({ "id": req.body.id }).deleteOne().exec();
+        res.send("Task deleted.")        
+    } catch (error) {        
+        console.log(error)
+    }
 });
 
 app.get("/", (req, res) => {
